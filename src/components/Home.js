4b51133c6d836f33b67519c28d76fd21e7365843
Jacob Handwerger <jstudyeth@gmail.com>
2023-08-14 11:30:03 -0400

    glass nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,23 @@
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from "react-router-hash-link";
+
 const Home = () => {
-    return (
-      <div id="home">
-      <div className='heading-txt'>
-          <h1>Hi, I'm Jacob</h1>
-          <div className='home-flex'>
-            <h2>A full stack developer passionate about learning and building new things</h2>
-            <HashLink className='contact-btn' to='#contact' smooth>Contact Me</HashLink>
-          </div>
+  return (
+    <div id="home">
+      <div className="heading-txt">
+        <h1>Hi, I'm Jacob</h1>
+        <div className="home-flex">
+          <h2>
+            A full stack developer passionate about learning and building new
+            things
+          </h2>
+          <HashLink className="contact-btn" to="#contact" smooth>
+            Contact Me
+          </HashLink>
         </div>
-        <img src={require('../pfp.jpg')} alt="pfp" />
-      </div> 
-    )
-  }
-  
-  export default Home;
+      </div>
+      <img src={require("../pfp.jpg")} alt="pfp" className="profile-image" />
+    </div>
+  );
+};
+
+export default Home;
